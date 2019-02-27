b2f79b3e7d15417496353821ff19fa7bd1f9a88f
Chris Leveille <cdleveille@gmail.com>
2019-02-26 22:25:36 -0500

    First commit - only Program.cs

---
+++ FilenameScrambler/Program.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilenameScrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory();
+                DirectoryInfo dir = new DirectoryInfo(path);
+                FileInfo[] files = dir.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    if (file.Extension != ".exe")
+                    {
+                        string filename = file.Name;
+                        string newFilename = Scramble(filename);
+
+                        File.Delete(path + "\\" + newFilename);
+                        File.Move(path + "\\" + filename, path + "\\" + newFilename);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static string Scramble(string filename)
+        {
+            Dictionary<char, char> charLookup = CreateCharLookup();
+            char[] filenameChar = filename.ToCharArray();
+            string toReturn = "";
+
+            foreach (char c in filenameChar)
+            {
+                if (charLookup.ContainsKey(c))
+                {
+                    toReturn += charLookup[c];
+                }
+                else
+                {
+                    toReturn += c;
+                }
+            }
+
+            return toReturn;
+        }
+
+        private static Dictionary<char, char> CreateCharLookup()
+        {
+            Dictionary<char, char> toReturn = new Dictionary<char, char>();
+
+            toReturn.Add('a', 'n');
+            toReturn.Add('b', 'o');
+            toReturn.Add('c', 'p');
+            toReturn.Add('d', 'q');
+            toReturn.Add('e', 'r');
+            toReturn.Add('f', 's');
+            toReturn.Add('g', 't');
+            toReturn.Add('h', 'u');
+            toReturn.Add('i', 'v');
+            toReturn.Add('j', 'w');
+            toReturn.Add('k', 'x');
+            toReturn.Add('l', 'y');
+            toReturn.Add('m', 'z');
+            toReturn.Add('n', 'a');
+            toReturn.Add('o', 'b');
+            toReturn.Add('p', 'c');
+            toReturn.Add('q', 'd');
+            toReturn.Add('r', 'e');
+            toReturn.Add('s', 'f');
+            toReturn.Add('t', 'g');
+            toReturn.Add('u', 'h');
+            toReturn.Add('v', 'i');
+            toReturn.Add('w', 'j');
+            toReturn.Add('x', 'k');
+            toReturn.Add('y', 'l');
+            toReturn.Add('z', 'm');
+            toReturn.Add('A', 'N');
+            toReturn.Add('B', 'O');
+            toReturn.Add('C', 'P');
+            toReturn.Add('D', 'Q');
+            toReturn.Add('E', 'R');
+            toReturn.Add('F', 'S');
+            toReturn.Add('G', 'T');
+            toReturn.Add('H', 'U');
+            toReturn.Add('I', 'V');
+            toReturn.Add('J', 'W');
+            toReturn.Add('K', 'X');
+            toReturn.Add('L', 'Y');
+            toReturn.Add('M', 'Z');
+            toReturn.Add('N', 'A');
+            toReturn.Add('O', 'B');
+            toReturn.Add('P', 'C');
+            toReturn.Add('Q', 'D');
+            toReturn.Add('R', 'E');
+            toReturn.Add('S', 'F');
+            toReturn.Add('T', 'G');
+            toReturn.Add('U', 'H');
+            toReturn.Add('V', 'I');
+            toReturn.Add('W', 'J');
+            toReturn.Add('X', 'K');
+            toReturn.Add('Y', 'L');
+            toReturn.Add('Z', 'M');
+            toReturn.Add('1', '6');
+            toReturn.Add('2', '7');
+            toReturn.Add('3', '8');
+            toReturn.Add('4', '9');
+            toReturn.Add('5', '0');
+            toReturn.Add('6', '1');
+            toReturn.Add('7', '2');
+            toReturn.Add('8', '3');
+            toReturn.Add('9', '4');
+            toReturn.Add('0', '5');
+
+            return toReturn;
+        }
+    }
+}
